46e4d542686ff92f345ed984670c007566687004
w32siooo <creeco@gmail.com>
2021-10-11 09:01:47 +0200

    set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './pages/admin.page/admin.page.component';
+import { ChatPageComponent } from './pages/chat.page/chat.page.component';
+import { LandingPageComponent } from './pages/landing.page/landing.page.component';
+import { PlayerPageComponent } from './pages/player.page/player.page.component';
+import { UserPageComponent } from './pages/user.page/user.page.component';
+import { ZombiePageComponent } from './pages/zombie.page/zombie.page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'chat', component: ChatPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'player', component: PlayerPageComponent },
+  { path: 'user', component: UserPageComponent },
+  { path: 'zombie', component: ZombiePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
